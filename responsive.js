9cf79407ecf61562618926aaf24443bce3bb0d4a
Akinjole Abass <akinjoleabass@gmail.com>
2025-07-02 14:38:16 -0700

    Initial commit

---
+++ responsive.js
@@ -0,0 +1,10 @@
+const navItem = document.getElementById("navItem");
+const menuIcon = document.getElementById("menuIcon");
+const openIcon = document.getElementById("openIcon");
+menuIcon.addEventListener("click", () => {
+    navItem.classList.toggle("show");
+})
+const submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", () => {
+    alert("Login Sucessful");
+})
